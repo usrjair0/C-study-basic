d55f772fd66eabe6ebf872116ef2ed9aac3c474c
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-01-31 10:16:48 -0300

    adicionado Senha program

---
+++ Senha/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==bem vindo ao programa Senha!==");
+
+            Console.Write("informe sua senha: ");
+            string senha = Console.ReadLine();
+
+            if(senha == "123")
+            {
+                Console.WriteLine("Senha correta!");
+                Console.WriteLine("você vai ter acesso a uma novidade!");
+            }
+                
+
+            Console.WriteLine("Digite Enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
