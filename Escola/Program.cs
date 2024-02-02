f7c2507b465b7b99a2af8d58bc4d694a350da4bf
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-02 09:01:47 -0300

    comments prof 2

--- Escola/Program.cs
+++ Escola/Program.cs
@@ -19,38 +19,43 @@ namespace Escola
             const double MEDIAAPROVACAO = 7.0;
             const double LIMITEFALTASPORCENTAGEM = 0.25;
             const int TOTALAULAS = 80;
-            
+            const int limiteFaltas = (int)(TOTALAULAS * LIMITEFALTASPORCENTAGEM); //conversão implicita
 
             Console.WriteLine("==bem vindo ao programa Escola!==");
 
-            Console.Write("Digite a primeira nota: ");
-            double nota1 = double.Parse(Console.ReadLine());
-
-            Console.Write("Digite a segunda nota: ");
-            double nota2 = double.Parse(Console.ReadLine());
-
-            Console.Write("Digite a terceira nota: ");
-            double nota3 = double.Parse(Console.ReadLine());
-
             Console.Write("Quantas faltas o aluno teve: ");
             double faltasAluno = double.Parse(Console.ReadLine());
-
-            double mediaNotas = (nota1 + nota2 + nota3) / 3;
-            const int limiteFaltas = (int) (TOTALAULAS * LIMITEFALTASPORCENTAGEM); //conversão implicita
 
             if (faltasAluno > limiteFaltas)
             {
                 Console.WriteLine($"Reprovado por falta! {faltasAluno}");
 
-            }else if (mediaNotas < MEDIAAPROVACAO)
-
-            {
-                Console.WriteLine($"A média foi {mediaNotas:0.0}. Reprovado por média! ");
             }else
             {
-                Console.WriteLine($"A média foi {mediaNotas:0.0}. Aprovado por média!");
-            }
+                
+                Console.Write("Digite a primeira nota: ");
+                double nota1 = double.Parse(Console.ReadLine());
 
+                Console.Write("Digite a segunda nota: ");
+                double nota2 = double.Parse(Console.ReadLine());
+
+                Console.Write("Digite a terceira nota: ");
+                double nota3 = double.Parse(Console.ReadLine());
+
+                double mediaNotas = (nota1 + nota2 + nota3) / 3;
+
+                if (mediaNotas < MEDIAAPROVACAO)
+
+                {
+                    Console.WriteLine($"A média foi {mediaNotas:0.0}. Reprovado por média! ");
+                }
+                else
+                {
+                    Console.WriteLine($"A média foi {mediaNotas:0.0}. Aprovado por média!");
+                }
+
+            }
+            
             Console.WriteLine("Pressione ENTER para encerrar o programa");
             Console.ReadLine();
         }
