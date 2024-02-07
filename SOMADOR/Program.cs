a46517e858e872af15db5f438f617ae95a8204ba
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-06 22:25:30 -0300

    somador add

---
+++ SOMADOR/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOMADOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa que leia três valores inteiros e imprima soma deles.
+            */
+            Console.WriteLine("---Bem vindo ao programa somador---");
+
+            Console.Write("Digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o terceiro número: ");
+            double num3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A soma é : {num1+num2+num3}");
+            Console.WriteLine("Digite enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
