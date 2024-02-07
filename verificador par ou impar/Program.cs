c3c5e343c5be86d021d5ffe3b653453019753bda
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-06 23:20:09 -0300

    verificador paridade

---
+++ verificador par ou impar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificador_par_ou_impar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Bem vindo ao programa verificador de paridade---");
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero%2 == 0)
+                Console.WriteLine("O número é par");
+            else
+                Console.WriteLine("O número é impar");
+
+            Console.WriteLine("Digite ENTER para encerrar");
+            Console.ReadLine();
+        }
+    }
+}
