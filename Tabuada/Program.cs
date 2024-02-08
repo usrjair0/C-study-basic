3b2746d2fac13e22f16e8c8bfca72e5bbf8ecd0f
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-08 09:41:54 -0300

    tabuada c#

---
+++ Tabuada/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Bem vindo ao programa TUBUADA---");
+            Console.Write("Informe o número da tabuada: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"tabuada do {numero}:");
+
+            /*for(int i = 0; i < 11; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }*/
+            int x = 0;
+            while( x < 11 ) 
+            {
+                Console.WriteLine($"{numero} x {x} = {numero * x}");
+                x++;
+            }
+
+            Console.WriteLine("Pressione Enter para fechar o programa");
+            Console.ReadLine(); 
+        }
+    }
+}
