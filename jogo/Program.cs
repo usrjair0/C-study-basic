4c7f626181ada4426aac110acd5de5b9edba835f
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-15 08:28:10 -0300

    initial commit

---
+++ jogo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==bem vindo ao programa Escola!\n==");
+
+
+
+            Console.WriteLine("\nPressione ENTER para encerrar o programa");
+            Console.ReadLine();
+        }
+    }
+}
